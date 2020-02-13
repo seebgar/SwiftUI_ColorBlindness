d02dc0fab23a16b726fba2b9acb2a33eae7ddd1f
Sebastian Garcia <js.garcial1@uniandes.edu.co>
2020-02-13 08:35:13 -0500

    Initial Commit

---
+++ Color Blind/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Color Blind
+//
+//  Created by Sebastian on 2/13/20.
+//  Copyright © 2020 Sebastian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
